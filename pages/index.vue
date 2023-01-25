f5c98c76f2739a1f8f71b8a2433ce15b0a3f3258
Niamh Gallagher <ngallagher@vtime.local>
2023-01-25 16:57:44 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <Hello />
+  <h1>This is the home page</h1>
+</template>
+
+<script>
+import Hello from '~~/components/Hello.vue';
+
+export default {
+  components: [
+    Hello,
+  ]
+}
+</script>
+
+<style>
+
+</style>
